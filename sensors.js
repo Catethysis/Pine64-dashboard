f1a6b3d3459e0a290b6b524b4e81f108c2d8d870
Catethysis <catethysis@gmail.com>
2018-08-29 02:24:57 +0300

    Numbers parsing corrected to be more obvious

--- sensors.js
+++ sensors.js
@@ -1,165 +1,163 @@
 let fs = require('fs');
 
 class sensor {
-	constructor (params) {
-		this.name = params.name;
-		this.path = params.path;
-		this.period = params.period || 1000;
-		this.buflen = params.buflen || 100;
-		this.buf = new Buffer.alloc(this.buflen);
-		fs.open(this.path, 'r', (err, fd) => {
-			if(!err) {
-				this.fd = fd;
-				this.process();
-			} else
-				throw new Error("Sensor " + this.path + " open error: \n\t" + err);
-		});
-	}
+  constructor (params) {
+    this.name = params.name;
+    this.path = params.path;
+    this.period = params.period || 1000;
+    this.buflen = params.buflen || 100;
+    this.buf = new Buffer.alloc(this.buflen);
+    fs.open(this.path, 'r', (err, fd) => {
+      if(!err) {
+        this.fd = fd;
+        this.process();
+      } else
+        throw new Error("Sensor " + this.path + " open error: \n\t" + err);
+    });
+  }
 
-	process() {
-		if(this.fd) {
-			fs.read(this.fd, this.buf, 0, this.buflen, 0, (err, count) => {
-				if(!err) {
-					this.fired = new Date();
-					let readedData = this.buf.toString('utf8', 0, count);
-					this.data = this.parse(readedData);
-				} else
-					throw new Error("Sensor " + this.path + " read error: " + err);
-			});
-		}
-		setTimeout(() => this.process(), this.period);
-		// console.log(this);
-		// console.log(this.data);
-	}
+  process() {
+    if(this.fd) {
+      fs.read(this.fd, this.buf, 0, this.buflen, 0, (err, count) => {
+        if(!err) {
+          this.fired = new Date();
+          let readedData = this.buf.toString('utf8', 0, count);
+          this.data = this.parse(readedData);
+        } else
+          throw new Error("Sensor " + this.path + " read error: " + err);
+      });
+    }
+    setTimeout(() => this.process(), this.period);
+  }
 
-	parse(data) {
-		return data;
-	}
+  parse(data) {
+    return data;
+  }
 }
 
 class temp extends sensor {
-	constructor () {
-		super({
-			name: 'temp',
-			path: '/sys/devices/virtual/thermal/thermal_zone0/temp',
-			buflen: 10
-		});
-	}
+  constructor () {
+    super({
+      name: 'temp',
+      path: '/sys/devices/virtual/thermal/thermal_zone0/temp',
+      buflen: 10
+    });
+  }
 
-	parse(data) {
-		return +data;
-	}
+  parse(data) {
+    return Number(data);
+  }
 }
 
 class uptime extends sensor {
-	constructor () {
-		super({
-			name: 'uptime',
-			path: '/proc/uptime',
-			buflen: 30
-		});
-	}
+  constructor () {
+    super({
+      name: 'uptime',
+      path: '/proc/uptime',
+      buflen: 30
+    });
+  }
 
-	parse(data) {
-		return +data.split(' ')[0];
-	}
+  parse(data) {
+    return Number(data.split(' ')[0]);
+  }
 }
 
 class cpuUsage extends sensor {
-	constructor (cores) {
-		super({
-			name: 'cpuUsage',
-			path: '/proc/stat',
-			buflen: 400
-		});
-		this.cores = cores;
-	}
+  constructor (cores) {
+    super({
+      name: 'cpuUsage',
+      path: '/proc/stat',
+      buflen: 400
+    });
+    this.cores = cores;
+  }
 
-	parse(data) {
-		return data.split('\n').slice(0, this.cores + 1).map((elem) => { //change 5 to real cores number
-			let fields = elem.split(/\s+/);
-			return {load: +fields[1] + +fields[3], total: +fields[1] + +fields[3] + +fields[4]};
-		});
-	}
+  parse(data) {
+    return data.split('\n').slice(0, this.cores + 1).map((elem) => { //change 5 to real cores number
+      let fields = elem.split(/\s+/).map(Number);
+      return {load: fields[1] + fields[3], total: fields[1] + fields[3] + fields[4]};
+    });
+  }
 }
 
 class cpuLA extends sensor {
-	constructor () {
-		super({
-			name: 'load_average',
-			path: '/proc/loadavg',
-			period: 1000,
-			buflen: 100
-		});
-	}
+  constructor () {
+    super({
+      name: 'load_average',
+      path: '/proc/loadavg',
+      period: 1000,
+      buflen: 100
+    });
+  }
 
-	parse(data) {
-		return data.split(' ').slice(0, 3).map(Number);
-	}
+  parse(data) {
+    return data.split(' ').slice(0, 3).map(Number);
+  }
 }
 
 class networkStat extends sensor {
-	constructor (iface, direction) {
-		super({
-			name: 'net' + direction.toUpperCase(),
-			path: '/sys/class/net/' + iface + '/statistics/' + direction + '_bytes',
-			period: 1000,
-			buflen: 20
-		});
-	}
+  constructor (iface, direction) {
+    super({
+      name: 'net' + direction.toUpperCase(),
+      path: '/sys/class/net/' + iface + '/statistics/' + direction + '_bytes',
+      period: 1000,
+      buflen: 20
+    });
+  }
 
-	parse(data) {
-		return +data;
-	}
+  parse(data) {
+    return Number(data);
+  }
 }
 
 class coreFreq extends sensor {
-	constructor (core) {
-		super({
-			name: 'coreFreq' + core,
-			path: '/sys/devices/system/cpu/cpu' + core + '/cpufreq/cpuinfo_cur_freq'
-		});
-	}
+  constructor (core) {
+    super({
+      name: 'coreFreq' + core,
+      path: '/sys/devices/system/cpu/cpu' + core + '/cpufreq/cpuinfo_cur_freq'
+    });
+  }
 
-	parse(data) {
-		return +data;
-	}
+  parse(data) {
+    return Number(data);
+  }
 }
 
 class voltage extends sensor {
-	constructor (channel) {
-		super({
-			name: 'voltage' + channel,
-			path: '/sys/class/regulator/regulator.' + channel + '/microvolts'
-		});
-	}
+  constructor (channel) {
+    super({
+      name: 'voltage' + channel,
+      path: '/sys/class/regulator/regulator.' + channel + '/microvolts'
+    });
+  }
 
-	parse(data) {
-		return +data / 1000000;
-	}
+  parse(data) {
+    return Number(data) / 1000000;
+  }
 }
 
 const cores = 4;
 const voltage_channels = 12;
 
 let sensors = [
-	new temp(),
-	new uptime(),
-	new cpuUsage(cores),
-	new cpuLA(),
-	new networkStat('eth0', 'rx'),
-	new networkStat('eth0', 'tx'),
+  new temp(),
+  new uptime(),
+  new cpuUsage(cores),
+  new cpuLA(),
+  new networkStat('eth0', 'rx'),
+  new networkStat('eth0', 'tx'),
 ];
 
 for(let i = 0; i < cores; i++)
-	sensors.push(new coreFreq(i));
+  sensors.push(new coreFreq(i));
 
 for(let i = 0; i < voltage_channels; i++)
-	sensors.push(new voltage(i + 1));
+  sensors.push(new voltage(i + 1));
 
 setInterval(() => {
-	sensors.forEach((sensor) => {
-		console.log(sensor.name + ': ' + sensor.data);
-	});
-	console.log();
+  sensors.forEach((sensor) => {
+    console.log(sensor.name + ': ' + sensor.data);
+  });
+  console.log();
 }, 1000);
